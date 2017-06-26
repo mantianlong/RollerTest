a5bd729f45c570a2c800882669a00822000c1711
mantianlong <770768556@qq.com>
2017-06-26 08:20:56 +0800

    满天龙20170626：完成开启试验

--- RollerTest/RollerTest.Domain/Concrete/EFTestreportinfoRepository.cs
+++ RollerTest/RollerTest.Domain/Concrete/EFTestreportinfoRepository.cs
@@ -21,5 +21,29 @@ namespace RollerTest.Domain.Concrete
                 return context.RollerTestreportInfos;
             }
         }
+        public void SaveRollerTestreportInfo(RollerTestreportInfo rollertestreportinfo)
+        {
+            if (rollertestreportinfo.RollerTestReportInfoID == 0)
+            {
+                context.RollerTestreportInfos.Add(rollertestreportinfo);
+            }
+            else
+            {
+                RollerTestreportInfo dbEntry = context.RollerTestreportInfos.Find(rollertestreportinfo.RollerTestReportInfoID);
+                if (dbEntry != null)
+                {
+                    dbEntry.RollerTestReportInfoID = rollertestreportinfo.RollerTestReportInfoID;
+                    dbEntry.RollerSampleInfoID = rollertestreportinfo.RollerSampleInfoID;
+                    dbEntry.StartStatus = rollertestreportinfo.StartStatus;
+                    dbEntry.StartText = rollertestreportinfo.StartText;
+                    dbEntry.EndText = rollertestreportinfo.EndText;
+                    dbEntry.EndStatus = rollertestreportinfo.EndStatus;
+                    dbEntry.FinalStatus = rollertestreportinfo.FinalStatus;
+                    dbEntry.StartTime = rollertestreportinfo.StartTime;
+                    dbEntry.EndTime = rollertestreportinfo.EndTime;
+                }
+            }
+            context.SaveChanges();
+        }
     }
 }
